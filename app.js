d64eed58bd86b4c419a4374699e0bee3c1b02685
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-16 22:22:41 +0530

    tsc autowatch and basic types

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('It works!');
+// string
+var myName = 'Ayush';
+// myName = 22;
+// number
+var myAge = 22.8;
+// myAge = 'asd';
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 23;
+// myRealAge = 'asd';
+// array
+var hobbies = ['cooking', 'sports'];
+hobbies = [100];
+// hobbies = 100;
+// tuples
+var address = ['street', 99];
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Grey"] = 0] = "Grey";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 101
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = 'bmw';
+console.log(car);
+car = { brand: 'bmw', series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -1,1 +1,39 @@
-console.log('It works!');
+// string
+let myName = 'Ayush';
+// myName = 22;
+
+// number
+let myAge = 22.8;
+// myAge = 'asd';
+
+// boolean
+let hasHobbies = true;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 23;
+// myRealAge = 'asd';
+
+// array
+let hobbies: any[] = ['cooking', 'sports'];
+hobbies = [100];
+// hobbies = 100;
+
+// tuples
+let address: [string, number] = ['street', 99];
+
+// enums
+enum Color {
+  Grey, //0
+  Green = 100,
+  Blue // 101
+}
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = 'bmw';
+console.log(car);
+car = { brand: 'bmw', series: 3 };
+console.log(car);
