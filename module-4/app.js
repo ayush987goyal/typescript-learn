97925cce72a4591dc8ced69ab522b0ba4f43c1eb
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-17 23:07:27 +0530

    adding module 4

--- module-4/app.js
+++ module-4/app.js
@@ -1,2 +1,8 @@
 "use strict";
-var myName = 'AYush';
+// Destucturing
+var myHobbbies = ['Cooking', 'Sports'];
+var ho1 = myHobbbies[0], ho2 = myHobbbies[1];
+console.log(ho1, ho2);
+var user = { name: 'asfsdf', age: 12 };
+var name = user.name, age = user.age;
+console.log(name, age);
--- module-4/app.ts
+++ module-4/app.ts
@@ -1,1 +1,8 @@
-let myName: string = 'AYush';
+// Destucturing
+const myHobbbies = ['Cooking', 'Sports'];
+const [ho1, ho2] = myHobbbies;
+console.log(ho1, ho2);
+
+const user = { name: 'asfsdf', age: 12 };
+const { name, age } = user;
+console.log(name, age);
