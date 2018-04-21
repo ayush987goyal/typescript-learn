5725159f8d9b45364902587d6b43f4d2b8a8052c
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-21 11:24:39 +0530

    added modules with systemJS

---
+++ module-6b/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var circle_1 = require("./math/circle");
+var rectangle_1 = __importDefault(require("./math/rectangle"));
+console.log(circle_1.PI);
+console.log(circle_1.calculateCircumference(10));
+console.log(rectangle_1.default(10, 20));
---
+++ module-6b/app.ts
@@ -0,0 +1,6 @@
+import { PI, calculateCircumference } from './math/circle';
+import calc from './math/rectangle';
+
+console.log(PI);
+console.log(calculateCircumference(10));
+console.log(calc(10, 20));
