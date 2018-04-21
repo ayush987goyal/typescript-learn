255e48b519aaeacb8a511f5ebab39439d4f8c0da
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-21 11:09:40 +0530

    adding namespace

---
+++ module-6a/app.js
@@ -0,0 +1,24 @@
+var MyMath;
+(function (MyMath) {
+    var Circle;
+    (function (Circle) {
+        var PI = 3.14;
+        function calculateCircumference(diameter) {
+            return diameter * PI;
+        }
+        Circle.calculateCircumference = calculateCircumference;
+    })(Circle = MyMath.Circle || (MyMath.Circle = {}));
+})(MyMath || (MyMath = {}));
+var MyMath;
+(function (MyMath) {
+    function calculateRectangle(width, length) {
+        return width * length;
+    }
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+var PI = 2.99;
+console.log(PI);
+console.log(MyMath.Circle.calculateCircumference(10));
+console.log(MyMath.calculateRectangle(10, 20));
---
+++ module-6a/app.ts
@@ -0,0 +1,8 @@
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+
+const PI = 2.99;
+
+console.log(PI);
+console.log(MyMath.Circle.calculateCircumference(10));
+console.log(MyMath.calculateRectangle(10, 20));
